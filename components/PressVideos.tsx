0c39d8b48f884dab41f99d5ea8c5c4b34598ef96
Om Sarraf <itsomsarraf+github@gmail.com>
2024-06-01 18:25:40 +0530

    done with changes

--- components/PressVideos.tsx
+++ components/PressVideos.tsx
@@ -19,51 +19,53 @@ const PressVideos = () => {
 
   return (
     <section id="videos" className="py-8 mt-12">
-      <div className="mx-auto">
+      <div className="container mx-auto px-4">
         <h2
           className={cn(
-            "text-center text-4xl font-bold underline underline-offset-8 decoration-blue-500 ",
+            "text-center text-4xl font-bold underline underline-offset-8 decoration-blue-500",
             montserrat.className
           )}
         >
           Featured Videos
         </h2>
-        <div className="flex flex-col gap-8 max-w-6xl mx-auto px-8 py-10">
+        <div className="flex flex-col gap-8 max-w-6xl mx-auto px-4 py-10">
           {videosData.slice(0, displayCount).map((video, index) => (
             <div
-              className="flex tracking-tight text-slate-100/50 w-full gap-4"
+              key={index}
+              className="flex flex-col md:flex-row tracking-tight text-slate-100/50 w-full gap-4"
               style={{
                 backgroundColor: "rgb(28, 28, 28)",
                 borderRadius: "20px",
                 padding: "20px",
               }}
             >
-              <div className="aspect-w-16 aspect-h-9">
+              <div className="aspect-w-16 aspect-h-9 w-full md:w-1/2">
                 <iframe
                   src={`https://www.youtube.com/embed/${video.videoId}?si=shYdzhDvsdFIoBT6`}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
+                  className="w-full h-full"
                 ></iframe>
               </div>
-              <div className="min-w-[240px] flex-1">
-                <h3 className="max-w-xs !pb-2 !m-0 font-bold text-xl text-base text-slate-100">
+              <div className="flex-1 p-2">
+                <h3 className="max-w-xs !m-0 font-bold text-base text-slate-100">
                   {video.title}
                 </h3>
-                <p>{video.description}</p>
+                <p className="text-sm md:text-base">{video.description}</p>
               </div>
             </div>
           ))}
         </div>
         {displayCount < videosData.length && (
           <div className="flex justify-center p-6">
-            <Button className=" px-4 py-2 rounded-md" onClick={loadMoreVideos}>
+            <Button className="px-4 py-2 rounded-md" onClick={loadMoreVideos}>
               Load More
             </Button>
           </div>
         )}
       </div>
-    </section>
+    </section >
   );
 };
 
